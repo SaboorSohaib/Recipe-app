90274dc92b5132e680fd5eb03aa049a06e66e936
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-07 18:37:00 +0430

    ADD users_id FOREIGN KEY TO FOOD TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_064254) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_140122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_064254) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_foods_on_users_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_064254) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users", column: "users_id"
 end
