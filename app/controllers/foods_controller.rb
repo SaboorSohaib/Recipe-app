a80731a1e85358d38ee793b025a84c4ce0f6dc95
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-08 10:21:39 +0430

    ADD CREATE AND NEW ACTION

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,22 @@
 class FoodsController < ApplicationController
   def index
+    @foods = current_user.foods
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @user = current_user
+    @food = @user.foods.new(foods_params)
+    redirect_to user_index_path(user_id: current_user.id) if @food.save
+  end
+
+
+  private
+
+  def foods_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
