922ca6767549c78bb8cb3ef22989946bc21e5201
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-08 20:06:07 +0430

    ADD FOOD

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,17 +5,18 @@ class FoodsController < ApplicationController
 
   def new
     @food = Food.new
+    @user = current_user
   end
 
   def create
     @user = current_user
-    @food = @user.foods.new(foods_params)
-    redirect_to user_index_path(user_id: current_user.id) if @food.save
+    @food = @user.foods.new(food_params)
+    redirect_to user_foods_index_path(user_id: @user.id) if @food.save
   end
 
   private
 
-  def foods_params
+  def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
-    resources :users, only:[:index] do
-      resources :foods, only:[:index, :new, :create, :destroy]
-    end
+  root "foods#index"
+  resources :foods, only:[:index, :new, :create, :destroy]
+    
 end
