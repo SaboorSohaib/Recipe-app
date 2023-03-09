48485a66badd258a3434833f5e4dc0bb73a94776
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 04:56:20 +0100

    MODIFY DATABASE CONFIGURATION

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root "users#index"
+  get 'recipes' => 'recipes#index'
+  resources :users do
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+    resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  end
 end
