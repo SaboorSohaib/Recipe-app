87aa4251b452452f14a67d0f4b50f55fd1990d58
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 05:18:56 +0100

    ADD create METHOD TO recipes_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,4 +11,17 @@ class RecipesController < ApplicationController
     def new
       @recipe = Recipe.new
     end
+  
+    # POST /recipes
+    def create
+      @recipe = Recipe.new(recipe_params)
+  
+      respond_to do |format|
+        if @recipe.save
+          format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
   end
