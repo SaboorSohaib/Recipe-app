a97a2f8a274d614ef7658cbd8166f4b2749489fb
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 05:18:11 +0100

    ADD new ACTION TO recipes_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,12 @@ class RecipesController < ApplicationController
     def index
       @recipes = Recipe.all
     end
+  
+    # GET /recipes/1
+    def show; end
+  
+    # GET /recipes/new
+    def new
+      @recipe = Recipe.new
+    end
   end
