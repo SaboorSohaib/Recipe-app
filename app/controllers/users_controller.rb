4a89a000fadf988c6b68dc911c8ce9557f95eeb0
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 05:08:34 +0100

    MODIFY UersController ACTION MESSAGES

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   
       respond_to do |format|
         if @user.save
-          format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
+          format.html { redirect_to user_url(@user), notice: 'New User has been successfully created.' }
         else
           format.html { render :new, status: :unprocessable_entity }
         end
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     def update
       respond_to do |format|
         if @user.update(user_params)
-          format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
+          format.html { redirect_to user_url(@user), notice: 'The User has been successfully updated.' }
         else
           format.html { render :edit, status: :unprocessable_entity }
         end
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
       @user.destroy
   
       respond_to do |format|
-        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.html { redirect_to users_url, notice: 'The User has been successfully destroyed.' }
       end
     end
   
