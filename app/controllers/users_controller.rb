c39a319c03f7d5c419c0aa6e5bfa87d55582aae0
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 05:06:52 +0100

    ADD update AND destroy METHODS TO UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,28 @@ class UsersController < ApplicationController
         end
       end
     end
+  
+    # PATCH/PUT /users/1
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # DELETE /users/1
+    def destroy
+      @user.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      end
+    end
+  
+    private
 
     # Use callbacks to share common setup
     def set_user
