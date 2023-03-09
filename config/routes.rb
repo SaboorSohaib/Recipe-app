aaaea99ab45f6c9276b30b0ac9391b6f30cdcd62
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 15:12:32 +0100

    MODIFY ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
-  resources :users, only:[:index] do 
+
+  resources :users, only:[:index, :new, :create, :update, :destroy] do 
     resources :food, only:[:index, :new, :create, :destroy]
     resources :recipes, only: [:index, :show, :new, :create, :destroy, :public]
   end
+
+  get 'public_recipes' => 'recipes#public_recipes'
 end
