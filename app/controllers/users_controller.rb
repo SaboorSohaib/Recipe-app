719c6011359de2d206d1b1f85cbcd4ae00fb8afd
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-09 05:04:10 +0100

    ADD index ACTION  TO UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
     before_action :set_user, only: %i[show edit update destroy]
+  
+    # GET /users
+    def index
+      @users = User.all
+    end
+
     # Use callbacks to share common setup
     def set_user
       @user = User.find(params[:id])
