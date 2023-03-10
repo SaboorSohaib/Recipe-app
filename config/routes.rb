f171e91ce01b89b422838edc60ada90f38592ac1
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-10 15:13:59 +0430

    ADD GRNERAL SHOPPING LIST ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   resources :users, only:[:index, :new, :create, :update, :destroy] do 
     resources :food, only:[:index, :new, :create, :destroy]
-    resources :recipes, only: [:index, :show, :new, :create, :destroy, :public]
+    resources :recipes, only: [:index, :show, :new, :create, :destroy, :public] do
+      resources :general_shopping_list, only: [:index]
+      end
   end
 
   get 'public_recipes' => 'recipes#public_recipes'
